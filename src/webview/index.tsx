5f473cc446e3139a80745bf52c87beef6dd2814f
Smit1963 <smit23492@gmail.com>
2025-04-21 01:07:07 +0530

    Initial Commit

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChatInterface } from '../components/ChatInterface';
+import ChatInterface from '../components/ChatInterface';
 
 declare global {
   interface Window {
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,75 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/webview/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: 'webview.js'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: 'tsconfig.json',
-              transpileOnly: true
+module.exports = [
+  {
+    // Extension host
+    target: 'node',
+    entry: './src/extension.ts',
+    output: {
+      path: path.resolve(__dirname, 'out'),
+      filename: 'extension.js',
+      libraryTarget: 'commonjs2'
+    },
+    externals: {
+      vscode: 'commonjs vscode'
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader'
             }
-          }
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+          ]
+        }
+      ]
+    },
+    devtool: 'source-map'
+  },
+  {
+    // Webview
+    target: 'web',
+    entry: './src/webview/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'out'),
+      filename: 'webview.js'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: 'tsconfig.json',
+                transpileOnly: true
+              }
+            }
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+      alias: {
+        'react': path.resolve('./node_modules/react'),
+        'react-dom': path.resolve('./node_modules/react-dom')
       }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    alias: {
-      'react': path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom')
+    },
+    performance: {
+      hints: false
     }
-  },
-  performance: {
-    hints: false
   }
-};
+];
